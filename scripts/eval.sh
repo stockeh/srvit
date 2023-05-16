ed1d51cadf596a1170c801168edcf5249c80ce2c
stockeh <jason12stock@gmail.com>
2023-05-16 14:09:04 -0600

    updated scripts

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 #
-# --------------------------
-#SBATCH --job-name="GREMLIN-ViT Eval"	 # job name
-#SBATCH --partition=peregrine-gpu 		 # partition to which job should be submitted
-#SBATCH --qos=gpu_short          		 # qos type
-#SBATCH --nodes=1                 		 # node count
-#SBATCH --cpus-per-gpu=6         		 # cpu-cores per gpu (>1 if multi-threaded tasks)
-#SBATCH --mem=64G              		     # total memory per node
-#SBATCH --gres=gpu:a100-sxm4-80gb:1      # Request GPU (A100 80GB)
-#SBATCH --time=00:30:00          	     # total run time limit (HH:MM:SS)
-#SBATCH --output=../logs/log.out         # output log file
-#SBATCH --error=../logs/log.err      	 # error file
-#SBATCH --mail-type=all        	         # send email 
+#SBATCH --job-name=ai2es_js
+#SBATCH -p a100
+#SBATCH --account=ai2es_premium
+#SBATCH -t 7-00:00                   # time limit: (D-HH:MM) 
+#SBATCH --gres=gpu:8
+#SBATCH --cpus-per-gpu=16            # --cpus-per-task=8
+#SBATCH --mem=128gb                  # --mem-per-cpu=1gb
+#SBATCH --nodes=1                    # Run all processes on a single node
+#SBATCH --output=../logs/log_%j.out  # output log file
+#SBATCH --error=../logs/log_%j.err   # error file
+#SBATCH --mail-type=all        	     # send email 
 #SBATCH --mail-user=stock@colostate.edu
 
-python ../src/main.py -m vit --cuda -b 16 --workers 8 --test --save
+apptainer exec --cleanenv --nv --bind /home/jstock:/home/jstock --bind /rdma/dgx-a100/jstock:/home/jstock/data /home/jstock/apptainer/DRIVE_PYTORCH/code_server_pytorch.sif /home/jstock/miniconda/envs/dev/bin/python ../src/main.py -m vit --cuda --test --save -b 48 --workers 8 --data-dir /home/jstock/data/conus3/A --h-depth 4 --h-heads 8
