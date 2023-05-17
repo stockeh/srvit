523a7b9426a554654a02513b53eea2aaba355e12
Jason Stock <jstock@XCITEMAIN.ASRC.ALBANY.EDU>
2023-05-17 15:40:44 +0000

    updated training script

--- train.sh
+++ train.sh
@@ -3,6 +3,7 @@
 #SBATCH --job-name=ai2es_js
 #SBATCH -p a100
 #SBATCH --account=ai2es_premium
+#SBATCH -t 7-00:00                   # time limit: (D-HH:MM) 
 #SBATCH --gres=gpu:8
 #SBATCH --cpus-per-gpu=16            # --cpus-per-task=8
 #SBATCH --mem=128gb                  # --mem-per-cpu=1gb
@@ -12,4 +13,4 @@
 #SBATCH --mail-type=all        	     # send email 
 #SBATCH --mail-user=stock@colostate.edu
 
-apptainer exec --cleanenv --nv --bind /home/jstock:/home/jstock --bind /rdma/dgx-a100/jstock:/home/jstock/data /home/jstock/apptainer/DRIVE_PYTORCH/code_server_pytorch.sif /home/jstock/miniconda/envs/dev/bin/python ../src/main.py -m vit --cuda --epochs 100 -b 24 --workers 8 --data-dir /home/jstock/data/conus3/A --h-depth 4 --h-heads 8
+apptainer exec --cleanenv --nv --bind /home/jstock:/home/jstock --bind /rdma/dgx-a100/jstock:/home/jstock/data /home/jstock/apptainer/DRIVE_PYTORCH/code_server_pytorch.sif /home/jstock/miniconda/envs/dev/bin/python ../src/main.py -m vit --cuda --epochs 200 -b 48 --workers 8 --data-dir /home/jstock/data/conus3/A/ --lr 0.0001 --h-depth 4 --h-heads 8
